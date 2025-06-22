471329a189f60df623bb9fca13fa6031e9d7215d
A-netrunner <halo37h117h@gmail.com>
2025-06-22 15:31:22 +0530

    modify express.json limit in index.js

--- backend/Src/index.js
+++ backend/Src/index.js
@@ -7,7 +7,7 @@ import cors from 'cors';
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-app.use(express.json())
+app.use(express.json({limit: "10mb"}))
 app.use(cors())
 // app.use(express.urlencoded({extended:true}))
 app.use('/api/auth',authRoute)
